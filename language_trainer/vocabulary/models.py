05f5b80b0cbabe89a5fa3b23ebb163e8ccb9cbd2
Jesaja Hur <j.hur@iqul.de>
2024-12-05 13:02:25 +0100

    created Tag and Card model

--- language_trainer/vocabulary/models.py
+++ language_trainer/vocabulary/models.py
@@ -1,30 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
-import iso639
 
 # Create your models here.
-class Language(models.Model):
-    # Load language choices into a list of tuples
-    LANGUAGES = [(code, iso639.to_name(code)) for code in iso639.langs]
-
-    code = models.CharField(max_length=2, unique=True)
-    name = models.CharField(max_length=100, blank=True)
-
-    def clean(self):
-        # Populate the name attribute in accordance to the code given
-        if not self.name:
-            self.name = dict(self.LANGUAGES).get(self.code)
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    users_using_tag = models.ManyToManyField(User)
 
     def __str__(self):
-        return f"{self.code} {self.name}"
-
-class Vocabulary(models.Model):
-    pass
+        return self.name
 
 
-class Tag(models.Model):
-    pass
+class Card(models.Model):
+    foreign_word = models.CharField(max_length=100)
+    translation = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tag, related_name='tagged_word')
+    users_using_word = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f"{self.foreign_word} - {self.translation}"
 
 
-class UserVocabulary(models.Model):
-    pass
